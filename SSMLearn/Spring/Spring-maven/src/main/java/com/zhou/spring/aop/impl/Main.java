34e86b57a72fea363756f4044a5898f469012a48
zhoufla <zero187735@163.com>
2017-09-27 14:54:34 +0800

    修改资源文件位置

--- SSMLearn/Spring/Spring-maven/src/main/java/com/zhou/spring/aop/impl/Main.java
+++ SSMLearn/Spring/Spring-maven/src/main/java/com/zhou/spring/aop/impl/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans-aop.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-aop/beans-aop.xml");
 
 		Calculator calculator = context.getBean("calculatorImpl", Calculator.class);
 		
